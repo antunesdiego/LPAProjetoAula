b15a42cfd704cf514aacf4f58654bebd58a3fdf1
Diego Antunes <diegoantunestreinador@gmail.com>
2024-03-14 11:57:52 -0300

    Aula de colisões de tiro, vida e dano

--- AdveturerShooter/code/Const.py
+++ AdveturerShooter/code/Const.py
@@ -1,9 +1,11 @@
 # C
 import pygame
 
-COLOR_ORANGE = (255, 158, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YELLOW = (255, 255, 128)
+C_ORANGE = (255, 158, 0)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 128)
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -29,6 +31,19 @@ ENTITY_SPEED = {'Level1Bg0': 0,
                 'Enemy2': 1,
                 'Enemy2Shot': 2}
 
+ENTITY_DAMAGE = {'Level1Bg0': 0,
+                 'Level1Bg1': 0,
+                 'Level1Bg2': 0,
+                 'Level1Bg3': 0,
+                 'Player1': 1,
+                 'Player1Shot': 25,
+                 'Player2': 1,
+                 'Player2Shot': 20,
+                 'Enemy1': 1,
+                 'Enemy1Shot': 20,
+                 'Enemy2': 1,
+                 'Enemy2Shot': 10}
+
 ENTITY_HEALTH = {'Level1Bg0': 999,
                  'Level1Bg1': 999,
                  'Level1Bg2': 999,
@@ -37,15 +52,28 @@ ENTITY_HEALTH = {'Level1Bg0': 999,
                  'Player1Shot': 1,
                  'Player2': 300,
                  'Player2Shot': 1,
-                 'Enemy1': 200,
-                 'Enemy1Shot': 5,
-                 'Enemy2': 200,
-                 'Enemy2Shot': 2}
+                 'Enemy1': 50,
+                 'Enemy1Shot': 1,
+                 'Enemy2': 60,
+                 'Enemy2Shot': 1}
+
+ENTITY_SCORE = {'Level1Bg0': 0,
+                'Level1Bg1': 0,
+                'Level1Bg2': 0,
+                'Level1Bg3': 0,
+                'Player1': 0,
+                'Player1Shot': 0,
+                'Player2': 0,
+                'Player2Shot': 0,
+                'Enemy1': 100,
+                'Enemy1Shot': 0,
+                'Enemy2': 125,
+                'Enemy2Shot': 0}
 
 ENTITY_SHOT_DELAY = {'Player1': 20,  # Intervalo de criação de Player1Shot quando a tecla de tiro for pressionada
                      'Player2': 15,
-                     'Enemy1' : 100,
-                     'Enemy2' : 200,
+                     'Enemy1': 100,
+                     'Enemy2': 200,
                      }
 
 # P
